e7df81b7bae0e39a938906748022e5ebce0f90d2
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-12-07 16:46:54 +0300

    style js -> jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './components/app/app.js';
+import App from './components/app/app.jsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
