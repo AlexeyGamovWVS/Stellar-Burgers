3773bfca505ce8e61ce63baffae985ea51f6aba3
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-12-14 13:22:17 +0300

    ref: изменить маппинг для ингредиентов и булки, обернуть вычисление в useMemo

--- src/components/main/burger-constructor/burger-components/burger-components.jsx
+++ src/components/main/burger-constructor/burger-components/burger-components.jsx
@@ -1,7 +1,7 @@
 import BurgerListItem from "../burger-list-item/burger-list-item";
 import burgCompStyles from "./burger-components.module.css";
 import { ChoiceContext } from "../../../../services/appContext";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import bunImage from "../../../../assets/images/default-bun.png";
 
 const EMPTY_BUN = {
@@ -10,66 +10,48 @@ const EMPTY_BUN = {
   image: bunImage,
 };
 
+const GET_RANDOM = () => {
+  return `${Math.floor(Math.random() * 999999999999999)}`;
+};
+
 export default function BurgerComponents() {
   const chosenItems = useContext(ChoiceContext);
-  let firstBun;
-  let lastBun;
-  const components = [];
+  const ingredients = useMemo(
+    () => chosenItems.filter((item) => item.type !== "bun"),
+    [chosenItems]
+  );
+	//надо ли для булки использовать useMemo?...
+  const bun = useMemo(
+    () => chosenItems.find((item) => item.type === "bun"),
+    [chosenItems]
+  );
 
-  chosenItems.forEach((element, index) => {
-    switch (element.type) {
-      case "bun":
-        firstBun = (
-          <BurgerListItem
-            item={element}
-            position="top"
-            iconVis={false}
-            key={element._id + index}
-          />
-        );
-        lastBun = (
-          <BurgerListItem
-            item={element}
-            position="bottom"
-            iconVis={false}
-            key={element._id + index + "bottom"}
-          />
-        );
-        break;
-      default:
-        components.push(
-          <BurgerListItem
-            item={element}
-            position="default"
-            iconVis={true}
-            key={element._id + index}
-          />
-        );
-    }
-  });
   return (
     <ul className={burgCompStyles.primaryList}>
-      {firstBun ? (
-        firstBun
-      ) : (
-        <BurgerListItem
-          item={EMPTY_BUN}
-          position="top"
-          iconVis={false}
-          key={Math.floor(Math.random() * 999999999999999)}
-        />
-      )}
-      <ul className={burgCompStyles.secondaryList}>{components.length > 0 && components}</ul>
-      {lastBun ? (
-        lastBun
-      ) : (
-        <BurgerListItem
-          item={EMPTY_BUN}
-          position="bottom"
-          iconVis={false}
-          key={Math.floor(Math.random() * 999999999999999)}
-        />
-      )}
+      <BurgerListItem
+        item={bun ? bun : EMPTY_BUN}
+        position="top"
+        iconVis={false}
+        key={bun ? bun._id : GET_RANDOM()}
+      />
+
+      <ul className={burgCompStyles.secondaryList}>
+        {ingredients.map((item, index) => (
+          <BurgerListItem
+            item={item}
+            position="default"
+            iconVis={true}
+            key={item._id + index}
+          />
+        ))}
+      </ul>
+
+      <BurgerListItem
+        item={bun ? bun : EMPTY_BUN}
+        position="bottom"
+        iconVis={false}
+        key={bun ? bun._id + GET_RANDOM() : GET_RANDOM()}
+      />
     </ul>
   );
 }
