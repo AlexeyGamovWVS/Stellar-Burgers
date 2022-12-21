1164b10fcd884126e66ac61b2b6118f4ff4a95ab
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-12-21 14:22:31 +0300

    feat: установить все необходимые пакеты, которые отвечают за работу Redux-хранилища приложения и DnD

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app.jsx";
+import { compose, createStore } from 'redux';
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
