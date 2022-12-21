a59320a16fbc30ae16030612372cd58ad38e08e6
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-12-21 14:39:15 +0300

    ref: инициализировать хранилище

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./components/app/app.jsx";
-import { compose, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./services/reducers/rootReducer.js";
+
 const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
-
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, enhancer);
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/services/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+const initialState = {
+	allItems: [],
+	selectedItems: [],
+	cuttentWatchItem: {},
+	oreder: {},
+}
+
+export const rootReducer = combineReducers({});
