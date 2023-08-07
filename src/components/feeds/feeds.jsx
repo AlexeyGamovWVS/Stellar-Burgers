5ab05ab4ea64731083cf4c5632f8e8f7adde31b3
Alexey Gamov <lexa2009xxxx@yandex.ru>
2023-08-07 20:50:01 +0300

    feat: вывести заказы и стату на стр. feed

--- src/components/feeds/feeds.jsx
+++ src/components/feeds/feeds.jsx
@@ -3,42 +3,41 @@ import OrderCard from "../orderCard/orderCard";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-export default function Feeds(orders) {
-  const orderNum = "54654654";
-  const orderDate = "2022-10-10T17:33:32.877Z";
-  const orderName = "Death Star Starship Main бургер";
-  const amountIngredients = 6;
-  const price = 54568;
-
-  const order = {
-    date: orderDate,
-    number: orderNum,
-    name: orderName,
-    ingredientsPictures: amountIngredients,
-    price: price,
-    key: orderNum,
-  };
-
+export default function Feeds() {
   const location = useLocation();
   const state = location.state;
 
   const { items } = useSelector((store) => store.allItems);
+  const { orders } = useSelector((store) => store.wsconnection);
+
+  const ordersMap = orders?.map((order) => {
+    const ingredientsPictures = order.ingredients.map(
+      (ingredient) => items.filter((storeItem) => storeItem._id === ingredient)[0].image
+    );
+    const totalPrice = order.ingredients
+      .map((ingredient) => items.filter((storeItem) => storeItem._id === ingredient)[0].price)
+      .reduce((acc, current) => {
+        return acc + current;
+      }, 0);
+
+    return (
+      <OrderCard
+        date={order.createdAt}
+        number={order.number}
+        name={order.name}
+        ingredientsPictures={ingredientsPictures}
+        price={totalPrice}
+        key={order.number}
+        state={state}
+        location={location}
+      ></OrderCard>
+    );
+  });
 
   return (
     <div className={styles.feeds}>
       <h2 className="text text_type_main-large mt-10 mb-5">Лента заказов</h2>
-      <ul className={`${styles.feeds__list} mt-10`}>
-        <OrderCard
-          date={order.date}
-          number={order.number}
-          name={order.name}
-          images={order.ingredientsPictures}
-          price={order.price}
-          key={order.number}
-          state={state}
-          location={location}
-        ></OrderCard>
-      </ul>
+      <ul className={`${styles.feeds__list} mt-10`}>{ordersMap && ordersMap}</ul>
     </div>
   );
 }
--- src/components/orderCard/orderCard.jsx
+++ src/components/orderCard/orderCard.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { CurrencyIcon, FormattedDate } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./orderCard.module.css";
+import { ORDER_STATUSES } from "../../utils/data";
+import ingredientsImagesMap from "./orderCard.utils";
 
 export default function OrderCard({
   date,
@@ -13,13 +15,14 @@ export default function OrderCard({
   location,
   status,
 }) {
-  const statusText =
-    status === "cancelled" ? "Отменен" : status === "done" ? "Выполнен" : "Готовится";
-
   state = { ...state, back: location };
+
   const getLinkAdress = () => {
     return location.pathname.startsWith("/feed") ? `/feed/${number}` : `/profile/orders/${number}`;
   };
+
+  const images = ingredientsImagesMap(ingredientsPictures, styles);
+
   return (
     <li className={`${styles.feeds__card} mr-2`} key={key}>
       <Link to={getLinkAdress()} className={styles.card} state={state}>
@@ -36,19 +39,22 @@ export default function OrderCard({
             className={
               status === "done"
                 ? `${styles.card__status} text text_type_main-small mt-2 ${styles.card__status_done}`
-								: status === 'cancelled' ? `${styles.card__status} text text_type_main-small mt-2 ${styles.card__status_canselled}`
+                : status === "cancelled"
+                ? `${styles.card__status} text text_type_main-small mt-2 ${styles.card__status_canselled}`
                 : `${styles.card__status} text text_type_main-small mt-2`
             }
           >
-            {statusText}
+            {status === ORDER_STATUSES.canselled
+              ? "Отменен"
+              : status === "done"
+              ? "Выполнен"
+              : "Готовится"}
           </p>
         ) : (
           <></>
         )}
         <div className={`${styles.card__total} mt-6`}>
-          <ul className={styles.card__ingredients}>
-            <li>Ingredient</li>
-          </ul>
+          <ul className={styles.card__ingredients}>{images}</ul>
           <div className={styles.card__pricebox}>
             <p className={`text text_type_digits-default`}>{price}</p>
             <CurrencyIcon />
